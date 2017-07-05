48f7388d3d5de84b24b0e904350c9773d0ed7426
Jack Shen <jack.shen@planwise.com>
2017-07-05 10:12:23 +1000

    first commit

---
+++ src/main/java/com/shenpinyi/thinkinhibernate/ThinkinhibernateApplication.java
@@ -0,0 +1,12 @@
+package com.shenpinyi.thinkinhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkinhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkinhibernateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shenpinyi/thinkinhibernate/ThinkinhibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shenpinyi.thinkinhibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThinkinhibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
